8c84197cafd68f886fa03f5eba55dfd85c396fb9
Амелия <amelia.mikhaylova17@gmail.com>
2025-10-04 14:58:46 +0300

    init commit

---
+++ lab/lab1/ArrayUtil.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ArrayUtil {
+    public static int[] inputArray(Scanner scanner) {
+        System.out.print("Введите количество элементов массива: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Введите элементы массива:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        return arr;
+    }
+    public static void printArray(int[] arr) {
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ lab/lab1/BinarySearch.java
@@ -0,0 +1,61 @@
+/*
+Реализовать алгоритм бинарного поиска двумя способами. Реализовать возможность ввода с клавиатуры и количество
+*/
+import java.util.Scanner;
+public class BinarySearch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] array = ArrayUtil.inputArray(scanner);
+
+        System.out.print("Введите число для поиска: ");
+        int target = scanner.nextInt();
+
+        System.out.println("Исходный массив:");
+        ArrayUtil.printArray(array);
+
+        int iterativeResult = binarySearchIterative(array, target);
+        if (iterativeResult == -1) {
+            System.out.println("Итеративный поиск: элемент не найден.");
+        } else {
+            System.out.println("Итеративный поиск: элемент находится на позиции " + (iterativeResult+1));
+        }
+
+        int recursiveResult = binarySearchRecursive(array, target, 0, array.length - 1);
+        if (recursiveResult == -1) {
+            System.out.println("Рекурсивный поиск: элемент не найден.");
+        } else {
+            System.out.println("Рекурсивный поиск: элемент находится на позиции " + (recursiveResult +1));
+        }
+    }
+    private static int binarySearchIterative(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int middle = (left + right) / 2;
+            if (arr[middle] == target) {
+                return middle;
+            } else if (arr[middle] < target) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+        return -1;
+    }
+    private static int binarySearchRecursive(int[] arr, int target, int left, int right) {
+        if (left > right) {
+            return -1;
+        }
+        int middle = (left + right) / 2;
+
+        if (arr[middle] == target) {
+            return middle;
+        } else if (arr[middle] < target) {
+            return binarySearchRecursive(arr, target, middle + 1, right);
+        } else {
+            return binarySearchRecursive(arr, target, left, middle - 1);
+        }
+    }
+}
+
