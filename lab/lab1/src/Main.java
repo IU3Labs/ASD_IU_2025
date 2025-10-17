c0bff71835b9194a303449e8419c50832c094ffe
asiya-kalab <k4labukhova@gmail.com>
2025-10-18 02:04:26 +0300

    Добавлено решение задачи В3

--- lab/lab1/src/Main.java
+++ lab/lab1/src/Main.java
@@ -1,12 +1,13 @@
 import tasks.task0.BinarySearchIterative;
 import tasks.task0.BinarySearchRecursive;
 import tasks.task1.TaskB1;
+import tasks.task2.TaskB3;
 
 void main() throws IOException {
     BinarySearchIterative.binarySearch();
     BinarySearchRecursive.binarySearch();
     TaskB1.taskB1();
-
+    TaskB3.taskB3();
 }
 
 
