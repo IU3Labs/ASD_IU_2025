bec82bfa6e8694ae9d560a2a62d026b0aa3c38d8
J0yFull <152690504+J0yFull@users.noreply.github.com>
2025-11-13 23:35:37 +0300

    Исправил ошибку

--- lab/lab2/lab-2/src/CircularQueue.java
+++ lab/lab2/lab-2/src/CircularQueue.java
@@ -5,23 +5,6 @@ import java.util.*;
 
 public class CircularQueue {
 
-    public static void main(String[] args) {
-        CircularQueue cq = new CircularQueue(5);
-        cq.enqueue(1);
-        cq.enqueue(2);
-        cq.enqueue(3);
-        cq.enqueue(4);
-        cq.enqueue(5); // очередь переполнена
-        cq.displayQueue();
-        cq.dequeue();
-        cq.dequeue();
-        cq.displayQueue();
-        cq.enqueue(6);
-        cq.enqueue(7);
-        cq.displayQueue();
-        System.out.println("Первый элемент: " + cq.peekFront());
-    }
-
     private int[] buffer;
     private int headIndex;
     private int tailIndex;
