863062afa3b743a1f31f88043a071e5c06a182a0
Maya1412nez <skorodinamaya@mail.ru>
2025-11-13 20:58:53 +0300

    Сделано базовое задание 2

---
+++ lab/lab2/Student.java
@@ -0,0 +1,16 @@
+package lab2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+
+public class Student {
+    Long id;
+    String name;
+    public Student(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
