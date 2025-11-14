c2329294c4b1c018865a6d9704dafa6684385348
Ruslan <ahtyamovrv@gmail.com>
2025-11-15 00:24:41 +0300

    init

---
+++ lab/lab1/BubbleSort.java
@@ -0,0 +1,21 @@
+package lab1;
+
+public class BubbleSort {
+    public static void bubbleSort(int[] array) {
+        int length = array.length;
+
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = 0; j < length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    swapElements(array, j, j + 1);
+                }
+            }
+        }
+    }
+
+    private static void swapElements(int[] array, int firstIndex, int secondIndex) {
+        int temp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = temp;
+    }
+}
---
+++ lab/lab1/PrintArray.java
@@ -0,0 +1,13 @@
+package lab1;
+
+public class PrintArray {
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i < array.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+    }
+}
