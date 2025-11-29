c2d2c1fc240b4b9b0ca2fef86c8f66a383514ddc
Константин <ziborov_kostya@mail.ru>
2025-11-29 15:46:02 +0300

    fix

--- lab/lab4/src/lab4/bellmanFord/BellmanFord.java
+++ lab/lab4/src/lab4/bellmanFord/BellmanFord.java
@@ -16,6 +16,7 @@ public class BellmanFord {
     static class ResultBellmanFord{
         List<Integer> path;
         long dist;
+        boolean hasNegativeCycle;
         ResultBellmanFord(){
             path = new ArrayList<>();
             dist = 0L;
@@ -34,7 +35,9 @@ public class BellmanFord {
 
         dist[start]=0;
 
+
         for (int i = 1; i < countNodes; i++) {
+
             boolean any = false;
             for (Edge e : edges) {
                 if (dist[e.from] != INF && dist[e.from] + e.dist < dist[e.to]) {
@@ -53,7 +56,13 @@ public class BellmanFord {
         result.path.addFirst(finish);
         result.dist = dist[finish];
         while(result.path.getFirst() != start){
-            result.path.addFirst(parent[result.path.getFirst()]);
+            int newElem = parent[result.path.getFirst()];
+            if (result.path.contains(newElem)){
+                result.hasNegativeCycle=true;
+                break;
+            }else {
+                result.path.addFirst(newElem);
+            }
         }
         return result;
     }
@@ -62,23 +71,31 @@ public class BellmanFord {
     public static void main(String[] args) {
         int countNodes = 4;
         List<Edge> edges = new ArrayList<>();
-        edges.add(new Edge(0, 1, 5));
-        edges.add(new Edge(0, 2, 4));
-        edges.add(new Edge(1, 3, 3));
-        edges.add(new Edge(2, 1, 6));
-        edges.add(new Edge(3, 2, 2));
+        edges.add(new Edge(0, 1, -5));
+        edges.add(new Edge(0, 2, -4));
+        edges.add(new Edge(1, 3, -3));
+        edges.add(new Edge(2, 1, -6));
+        edges.add(new Edge(3, 2, -2));
 
         ResultBellmanFord result = bellmanFord(countNodes, edges, 0, 3);
 
-        System.out.println("Дистанция 0->3: " + result.dist); // 8
-        System.out.println("Путь: " + result.toString()); // [0, 1, 3]
+        System.out.println("Дистанция 0->3");
+        if (result.hasNegativeCycle){
+            System.out.println("Найден цикл");
+        }else {
+            System.out.println("Путь: " + result.toString());
+        }
 
-        result = bellmanFord(countNodes, edges, 0, 0);
-        System.out.println("Дистанция 0->0: " + result.dist); // 0
-        System.out.println("Путь: " + result.toString()); // [0]
 
-        result = bellmanFord(countNodes, edges, 3, 0);
-        System.out.println("Дистанция 0->0: " + result.dist); // 0
-        System.out.println("Путь: " + result.toString()); // [] - путь не найден
+//        System.out.println("Дистанция 0->3: " + result.dist); // 8
+//        System.out.println("Путь: " + result.toString()); // [0, 1, 3]
+
+//        result = bellmanFord(countNodes, edges, 0, 0);
+//        System.out.println("Дистанция 0->0: " + result.dist); // 0
+//        System.out.println("Путь: " + result.toString()); // [0]
+//
+//        result = bellmanFord(countNodes, edges, 3, 0);
+//        System.out.println("Дистанция 0->0: " + result.dist); // 0
+//        System.out.println("Путь: " + result.toString()); // [] - путь не найден
     }
 }
