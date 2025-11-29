42562471e76cf918e2969ddbe5a4706dd71c8d85
DJTabyretka <pavelrumar07@gmail.com>
2025-11-29 16:52:15 +0300

    Update IterativeDeepeningDepthFirstSearch.java

--- lab/lab4/IterativeDeepeningDepthFirstSearch.java
+++ lab/lab4/IterativeDeepeningDepthFirstSearch.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class IterativeDeepeningDepthFirstSearch {
 
-        public static void main() {
+        public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
             Tree tree = Utility.createTree(scanner);
 
