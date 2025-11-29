eaf1c7df6f6837bdd505f6ec90486cbad5adb78a
ag0815923-ai <ag0815923@gmail.com>
2025-11-29 09:01:17 +0300

    лр1 опять

--- lab/lab1/src/ElementFrequency.java
+++ lab/lab1/src/ElementFrequency.java
@@ -6,29 +6,47 @@
 public class ElementFrequency {
     public static void main(String[] args) {
         int[] array = ArrayMethods.newArray();
-
-        int maxNumber = 0;
-        for(int i: array){
-            if(i > maxNumber){
-                maxNumber = i;
-            }
-        }
-
-        int[] frequency = new int[maxNumber + 1];
-
-        for(int i: array){
-            frequency[i]++;
-        }
-
-        int result = -1;
-        int length = frequency.length;
-        for(int i = 1; i <= length; i++){
-            if(i <= maxNumber && i == frequency[i] && result < i){
-                result = i;
-            }
-        }
+        int result = findNumberWithFrequencyEqualToValue(array);
         System.out.println(result);
     }
 
-}
+    public static int findNumberWithFrequencyEqualToValue(int[] array) {
+        if (array == null || array.length == 0) {
+            return -1;
+        }
 
+        int maxValue = findMaxValue(array);
+        int[] frequency = calculateFrequency(array, maxValue);
+        return findResult(frequency);
+    }
+
+    private static int findMaxValue(int[] array) {
+        int maxValue = array[0];
+        for (int i : array) {
+            if (i > maxValue) {
+                maxValue = i;
+            }
+        }
+        return maxValue;
+    }
+
+    private static int[] calculateFrequency(int[] array, int maxValue) {
+        int[] frequency = new int[maxValue + 1];
+        for (int i : array) {
+            if (i >= 0 && i <= maxValue) {
+                frequency[i]++;
+            }
+        }
+        return frequency;
+    }
+
+    private static int findResult(int[] frequency) {
+        int result = -1;
+        for (int i = 1; i < frequency.length; i++) {
+            if (i == frequency[i] && i > result) {
+                result = i;
+            }
+        }
+        return result;
+    }
+}
