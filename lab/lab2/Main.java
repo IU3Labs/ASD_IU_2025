09b87849b028de045dca2e1182b14efec0d07451
Maya1412nez <skorodinamaya@mail.ru>
2025-11-29 13:33:06 +0300

    ы
    ы

--- lab/lab2/Main.java
+++ lab/lab2/Main.java
@@ -14,7 +14,7 @@ public class Main {
         HashMap<Long, Student> mapa = new HashMap<>();
 
         for (long i = 0; i < SIZE; i++) {
-            Student student = new Student(i, "Bob");
+            Student student = new Student(i, "чел");
             arrlist.add(student);
             linklist.add(student);
             sett.add(student);
@@ -30,37 +30,37 @@ public class Main {
     public static void TestArray(ArrayList<Student> arrlist) {
         System.out.println("-----Тестирование ArrayList-----");
 
-        // Добавление 1 несуществующего элемента в конец
+        // 1 в конец
         long startTime = System.nanoTime();
         arrlist.add(new Student(10000001L, "New_Student")); // id = 10 000 001
         long endTime = System.nanoTime();
         System.out.println("Добавление в конец: " + (endTime - startTime) + " нс");
 
-        // Добавление 1 несуществующего элемента в начало.
+        // 1 в начало
         startTime = System.nanoTime();
         arrlist.addFirst(new Student(10000002L, "New_Student_Start"));
         endTime = System.nanoTime();
         System.out.println("Добавление в начало: " + (endTime - startTime) + " нс");
 
-        // Удаление последнего элемента
+        // Удаление последнего
         startTime = System.nanoTime();
         arrlist.removeLast();
         endTime = System.nanoTime();
         System.out.println("Удаление последнего: " + (endTime - startTime) + " нс");
 
-        // Удаление первого элемента
+        // Удаление первого
         startTime = System.nanoTime();
         arrlist.removeFirst();
         endTime = System.nanoTime();
         System.out.println("Удаление первого: " + (endTime - startTime) + " нс");
 
-        // Взятие (Get) центрального элемента
+        // Гет центрального
         startTime = System.nanoTime();
         Student middleElement = arrlist.get(SIZE / 2);
         endTime = System.nanoTime();
         System.out.println("Получение центрального: " + (endTime - startTime) + " нс");
 
-        // Взятие (Get) последнего элемента
+        // Гет последнего элемента
         startTime = System.nanoTime();
         Student lastElement = arrlist.getLast();
         endTime = System.nanoTime();
@@ -72,37 +72,37 @@ public class Main {
 public static void TestLink(LinkedList<Student> linklist) {
     System.out.println("\n-----Тестирование LinkedList-----");
 
-    // Добавление 1 несуществующего элемента в конец
+    // 1 в конец
     long startTime = System.nanoTime();
     linklist.add(new Student(10000001L, "New_Student")); // id = 10 000 001
     long endTime = System.nanoTime();
     System.out.println("Добавление в конец: " + (endTime - startTime) + " нс");
 
-    // Добавление 1 несуществующего элемента в начало.
+    // 1 в начало.
     startTime = System.nanoTime();
     linklist.addFirst(new Student(10000002L, "New_Student_Start"));
     endTime = System.nanoTime();
     System.out.println("Добавление в начало: " + (endTime - startTime) + " нс");
 
-    // Удаление последнего элемента
+    // Удаление последнего
     startTime = System.nanoTime();
     linklist.removeLast();
     endTime = System.nanoTime();
     System.out.println("Удаление последнего: " + (endTime - startTime) + " нс");
 
-    // Удаление первого элемента
+    // Удаление первого
     startTime = System.nanoTime();
     linklist.removeFirst();
     endTime = System.nanoTime();
     System.out.println("Удаление первого: " + (endTime - startTime) + " нс");
 
-    // Взятие (Get) центрального элемента
+    // Гет центрального
     startTime = System.nanoTime();
     Student middleElement = linklist.get(SIZE / 2);
     endTime = System.nanoTime();
     System.out.println("Получение центрального: " + (endTime - startTime) + " нс");
 
-    // Взятие (Get) последнего элемента
+    // Гет последнего
     startTime = System.nanoTime();
     Student lastElement = linklist.getLast();
     endTime = System.nanoTime();
@@ -113,37 +113,37 @@ public static void TestLink(LinkedList<Student> linklist) {
     public static void TestSet (HashSet<Student> sett) {
         System.out.println("\n-----Тестирование HashSet-----");
 
-        // Добавление 1 несуществующего элемента в конец
+        // 1 в конец
         long startTime = System.nanoTime();
         sett.add(new Student(10000001L, "New_Student")); // id = 10 000 001
         long endTime = System.nanoTime();
         System.out.println("Добавление в конец: " + (endTime - startTime) + " нс");
 
-        // Добавление 1 несуществующего элемента в начало.
+        // 1 в начало.
         startTime = System.nanoTime();
         sett.add(new Student(10000002L, "New_Student_Start"));
         endTime = System.nanoTime();
         System.out.println("Добавление в начало: " + (endTime - startTime) + " нс");
 
-        // Удаление последнего элемента
+        // Удаление последнего
         startTime = System.nanoTime();
         sett.remove((long) SIZE-1);
         endTime = System.nanoTime();
         System.out.println("Удаление последнего: " + (endTime - startTime) + " нс");
 
-        // Удаление первого элемента
+        // Удаление первого
         startTime = System.nanoTime();
         sett.remove((long) 0);
         endTime = System.nanoTime();
         System.out.println("Удаление первого: " + (endTime - startTime) + " нс");
 
-        // Взятие (Get) центрального элемента
+        // Гет центрального
         startTime = System.nanoTime();
         sett.contains((long) SIZE/2);
         endTime = System.nanoTime();
         System.out.println("Получение центрального: " + (endTime - startTime) + " нс");
 
-        // Взятие (Get) последнего элемента
+        // Гет последнего
         startTime = System.nanoTime();
         sett.contains((long) SIZE-1);
         endTime = System.nanoTime();
@@ -154,37 +154,37 @@ public static void TestLink(LinkedList<Student> linklist) {
     public static void TestMap (HashMap<Long, Student> mapa) {
         System.out.println("\n-----Тестирование HashMap-----");
 
-        // Добавление 1 несуществующего элемента в конец
+        // 1 в конец
         long startTime = System.nanoTime();
         mapa.put((long) SIZE + 1, new Student(10000001L, "New_Student"));
         long endTime = System.nanoTime();
         System.out.println("Добавление в конец: " + (endTime - startTime) + " нс");
 
-        // Добавление 1 несуществующего элемента в начало.
+        // 1 в начало.
         startTime = System.nanoTime();
         mapa.put((long) 0, new Student(10000002L, "New_Student"));
         endTime = System.nanoTime();
         System.out.println("Добавление в начало: " + (endTime - startTime) + " нс");
 
-        // Удаление последнего элемента
+        // Удаление последнего
         startTime = System.nanoTime();
         mapa.remove((long) SIZE-1);
         endTime = System.nanoTime();
         System.out.println("Удаление последнего: " + (endTime - startTime) + " нс");
 
-        // Удаление первого элемента
+        // Удаление первого
         startTime = System.nanoTime();
         mapa.remove((long) 0);
         endTime = System.nanoTime();
         System.out.println("Удаление первого: " + (endTime - startTime) + " нс");
 
-        // Взятие (Get) центрального элемента
+        // гет центрального
         startTime = System.nanoTime();
         mapa.get((long) SIZE / 2);
         endTime = System.nanoTime();
         System.out.println("Получение центрального: " + (endTime - startTime) + " нс");
 
-        // Взятие (Get) последнего элемента
+        // гет последнего
         startTime = System.nanoTime();
         mapa.get((long) SIZE-1);
         endTime = System.nanoTime();
