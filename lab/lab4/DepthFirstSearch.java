3a8468c48de7a2a18420fd37086509dccd82eb1f
DJTabyretka <pavelrumar07@gmail.com>
2025-11-29 16:50:12 +0300

    Update DepthFirstSearch.java

--- lab/lab4/DepthFirstSearch.java
+++ lab/lab4/DepthFirstSearch.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class DepthFirstSearch {
-    public static void main() {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Tree tree = Utility.createTree(scanner);
         int target = Utility.inputTarget(scanner);
