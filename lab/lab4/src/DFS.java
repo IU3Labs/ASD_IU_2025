a9daa35cde9fc5535b7491d215d799267970ba38
Артём Ковалев <artiomkratov@gmail.com>
2025-11-29 09:12:46 +0300

    fix: DFS traverse -> search

--- lab/lab4/src/DFS.java
+++ lab/lab4/src/DFS.java
@@ -4,35 +4,47 @@
 
 import structures.Node;
 
+import java.util.Scanner;
 import java.util.Stack;
 
 public class DFS {
 
-    public static <T> void recursiveTraverse(Node<T> node) {
+    public static <T> Node<T> recursiveSearch(Node<T> node, T valueToFind) {
         if (node == null) {
-            return;
-        };
-        System.out.printf(node.value + " ");
+            return null;
+        }
+        if (node.value.equals(valueToFind)) {
+            return node;
+        }
         for (Node<T> child : node.children) {
-            recursiveTraverse(child);
-        };
+            Node<T> result = recursiveSearch(child, valueToFind);
+            if (result != null) {
+                return result;
+            }
+        }
+        return null;
     };
 
-    public static <T> void iterativeTraverse(Node<T> root) {
+    public static <T> Node<T> iterativeSearch(Node<T> root, T valueToFind) {
         if (root == null) {
-            return;
-        };
+            return null;
+        }
+
         Stack<Node<T>> stack = new Stack<>();
         stack.push(root);
 
         while (!stack.isEmpty()) {
             Node<T> currentNode = stack.pop();
-            System.out.printf(currentNode.value + " ");
+            if (currentNode.value.equals(valueToFind)) {
+                return currentNode;
+            }
             for (int i = currentNode.children.size() - 1; i >= 0; i--) {
                 stack.push(currentNode.children.get(i));
-            };
-        };
-    };
+            }
+        }
+
+        return null;
+    }
 
     public static void main(String[] args) {
         Node<Integer> root = new Node<>(1);
@@ -51,10 +63,16 @@ public class DFS {
         node3.addChild(node7);
         node4.addChild(node8);
 
-        System.out.println("Рекурсивный обход в глубину:");
-        recursiveTraverse(root);
-        System.out.println("\nРекурсивный обход в глубину:");
-        iterativeTraverse(root);
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите значение, которое нужно найти: ");
+        int searchValue = in.nextInt();
+
+        System.out.println("Рекурсивный поиск в глубину:");
+        Node<Integer> recResult = recursiveSearch(root, searchValue);
+        System.out.println(recResult != null ? "Найдено" : "Не найдено");
+        System.out.println("\nИтеративный поиск в глубину:");
+        Node<Integer> iterResult = iterativeSearch(root, searchValue);
+        System.out.println(iterResult != null ? "Найдено" : "Не найдено");
     };
 
 };
