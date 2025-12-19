99f183c3d05e045dfd92b70187eb2ceac7e0c2df
doodos96 <139282951+doodos96@users.noreply.github.com>
2025-12-19 20:25:07 +0300

    files

---
+++ lab/lab3/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("А1)");
+        InversionCount.main(args);
+        System.out.println();
+
+        System.out.println("B1.1)");
+        MergeKLists.main(args);
+        System.out.println();
+
+        System.out.println("B2.1)");
+        TopKFrequentNlogN.main(args);
+        System.out.println();
+    }
+}
