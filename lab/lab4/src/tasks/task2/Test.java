884e0d8fcd3282d325c0e9c875e9b032e101dbf4
asiya-kalab <k4labukhova@gmail.com>
2025-12-20 12:16:38 +0300

    Update Test.java

--- lab/lab4/src/tasks/task2/Test.java
+++ lab/lab4/src/tasks/task2/Test.java
@@ -6,44 +6,22 @@ package tasks.task2;
 
 import java.util.Scanner;
 
+import tasks.helper.CreateTree;
+import tasks.helper.TreeNode;
+
 public class Test {
-
-    /**
-     * Создает тестовое дерево для демонстрации работы алгоритмов
-     * Структура тестового дерева:
-     *        1
-     *      / | \
-     *     2  3  4
-     *    / \    |
-     *   5   6   7
-     *       |
-     *       8
-     */
-    private static TreeNode createTestTree() {
-        TreeNode node1 = new TreeNode(1);
-        TreeNode node2 = new TreeNode(2);
-        TreeNode node3 = new TreeNode(3);
-        TreeNode node4 = new TreeNode(4);
-        TreeNode node5 = new TreeNode(5);
-        TreeNode node6 = new TreeNode(6);
-        TreeNode node7 = new TreeNode(7);
-        TreeNode node8 = new TreeNode(8);
-
-        node1.addChildren(node2, node3, node4);
-        node2.addChildren(node5, node6);
-        node4.addChild(node7);
-        node6.addChild(node8);
-
-        return node1;
-    }
 
     public static void main() {
         System.out.println("_______ ТЕСТИРОВАНИЕ BFS ДЛЯ ДЕРЕВА ________\n");
+        Scanner sc = new Scanner(System.in);
 
-        TreeNode root = createTestTree();
+
+        TreeNode root = null;
+        root = CreateTree.createTreeFromKeyboard(sc);
+        System.out.println("\nСтруктура созданного дерева:");
+        CreateTree.printTree(root, "", true);
 
         System.out.print("Введите искомое значение: ");
-        Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
         testSearch(root, number);
         sc.close();
