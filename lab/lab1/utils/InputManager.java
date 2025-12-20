148b17be8d26cf0876e31616c7c5a202daa4bb84
FaZAxON <timakinvlad@gmail.com>
2025-12-20 12:47:48 +0300

    init commit

---
+++ lab/lab1/utils/InputManager.java
@@ -0,0 +1,23 @@
+package lab1.utils;
+import java.util.Scanner;
+
+public class InputManager {
+    private static Scanner scanner;
+
+
+    public static int inputTarget(Scanner scan) {
+        return scan.nextInt();
+    }
+
+    public static String readWord(Scanner scan) {
+        return scan.nextLine();
+    }
+
+
+    public static Scanner getScanner() {
+        if (scanner == null) {
+            scanner = new Scanner(System.in);
+        }
+        return scanner;
+    }
+}
