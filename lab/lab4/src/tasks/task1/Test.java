93b8bc3d79eb1b8d2796de2258e8e45f414fdfe8
asiya-kalab <k4labukhova@gmail.com>
2025-12-20 12:15:38 +0300

    Update Test.java

--- lab/lab4/src/tasks/task1/Test.java
+++ lab/lab4/src/tasks/task1/Test.java
@@ -4,54 +4,31 @@
 
 package tasks.task1;
 
+import tasks.helper.CreateTree;
+
 import java.util.Scanner;
 
+import tasks.helper.CreateTree;
+import tasks.helper.TreeNode;
+
+
 public class Test {
-
-    /**
-     * Создаем тестовое дерево для демонстрации работы алгоритмов
-     * Структура тестового дерева:
-     *        1
-     *      / | \
-     *     2  3  4
-     *    / \    |
-     *   5   6   7
-     *       |
-     *       8
-     */
-    private static TreeNode createTestTree() {
-        TreeNode node1 = new TreeNode(1);
-        TreeNode node2 = new TreeNode(2);
-        TreeNode node3 = new TreeNode(3);
-        TreeNode node4 = new TreeNode(4);
-        TreeNode node5 = new TreeNode(5);
-        TreeNode node6 = new TreeNode(6);
-        TreeNode node7 = new TreeNode(7);
-        TreeNode node8 = new TreeNode(8);
-
-        node1.addChildren(node2, node3, node4);
-        node2.addChildren(node5, node6);
-        node4.addChild(node7);
-        node6.addChild(node8);
-
-        return node1;
-    }
-
     public static void main() {
         System.out.println("_________ТЕСТИРОВАНИЕ DFS ДЛЯ ДЕРЕВА _________\n");
 
         // Создаем тестовое дерево
-        TreeNode root = createTestTree();
+        Scanner sc = new Scanner(System.in);
+
+
+        TreeNode root = null;
+        root = CreateTree.createTreeFromKeyboard(sc);
+        System.out.println("\nСтруктура созданного дерева:");
+        CreateTree.printTree(root, "", true);
 
         System.out.print("Введите искомое значение: ");
-        Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
-        testSearch(root, number);
-        sc.close();
-    }
+        int target = sc.nextInt();
 
-    // Тестирует поиск элемента обоими методами
-    private static void testSearch(TreeNode root, int target) {
+        // Тестирует поиск элемента обоими методами
         TreeNode recursiveResult = RecursiveDFS.search(root, target);
         TreeNode iterativeResult = IterativeDFS.search(root, target);
         System.out.println("Поиск " + " (" + target + "):");
@@ -59,6 +36,9 @@ public class Test {
         System.out.println("  Итеративный: " + iterativeResult);
         System.out.println("  Результаты совпадают: " +
                 (recursiveResult == null ? iterativeResult == null : recursiveResult.value == iterativeResult.value));
+
+
+        sc.close();
     }
 
 }
